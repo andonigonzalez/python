e8f7d5c32e5cd66bfd0b7fa55f38b42899ba1f87
unknown <zitup3@zd03-PC.CORP.ZITUDONOSTI>
2015-04-15 16:16:15 +0200

    todo actualizado

--- login.py
+++ login.py
@@ -2,13 +2,15 @@ nombre = "andoni"
 password = "123"
 num = 0
 
-while 0 < 1:
+while num < 3:
 	inombre = input("introduce el nombre de usuario: ")
 	ipassword = input("introduce la contraseña: ")
-	
 	if inombre == nombre and ipassword == password:
-		num += 1
 		print ("Bienvenido andoni")
 		break
 	else:
 		print ("Nombre o contraseña incorrectos")
+		num += 1
+		
+if num == 3:
+	print("Logeo fallido vuelva a intentarlo mas tarde")
